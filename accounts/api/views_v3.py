4ba949e6c83e4c26bb406f351f63008d38fc7076
thisishamza <f168345@nu.edu.pk>
2021-10-14 11:44:30 +0500

    Add customer permission

--- accounts/api/views_v3.py
+++ accounts/api/views_v3.py
@@ -1,16 +1,24 @@
 from accounts.models import Account
 from .serializers import AccountSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, BasePermission
+
+
+class CustomerAccessPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        if request.user.type == 'customer':
+            return True
 
 
 class AccountListCreateAPI(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [CustomerAccessPermission]
 
 
 class AccountRetrieveUpdateDestroyAPI(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser, CustomerAccessPermission]
