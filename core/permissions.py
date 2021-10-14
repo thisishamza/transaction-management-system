573ea0b20be86b1bc3aa7a89ae706251b20825d1
thisishamza <f168345@nu.edu.pk>
2021-10-14 13:10:34 +0500

    Update permissions file with UserTypeChoices

--- core/permissions.py
+++ core/permissions.py
@@ -1,7 +1,8 @@
 from rest_framework.permissions import BasePermission
+from users.models import UserTypeChoices
 
 
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
-        if request.user.type == 'customer':
+        if request.user.type == UserTypeChoices.CUSTOMER:
             return True
