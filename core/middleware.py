59d105e80fd1015ccc2b6057db7d37fbbe4f8dd7
thisishamza <f168345@nu.edu.pk>
2021-10-20 12:13:18 +0500

    Replace list with string startswith function

--- core/middleware.py
+++ core/middleware.py
@@ -5,8 +5,6 @@ from random import randint
 request_logger = logging.getLogger('django.request')
 error_logger = logging.getLogger('django.request')
 
-error_status_code_list = [400, 403, 404, 405, 406, 408, 412, 431, 500, 501, 503]
-
 
 class RequestLogMiddleware:
     def __init__(self, get_response):
@@ -26,7 +24,7 @@ class RequestLogMiddleware:
         if "/api" in request.get_full_path():
             req_body = json.loads(request.body) if request.body else {}
             log_data["request_body"] = req_body
-            if response.status_code not in error_status_code_list:
+            if not str(response.status_code).startswith("4") or str(response.status_code).startswith("5"):
                 response_obj = {
                     "request_id": request_id,
                     "request_body": req_body,
@@ -52,7 +50,7 @@ class ErrorLogMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
         if "/api" in request.get_full_path():
-            if response.status_code in error_status_code_list:
+            if str(response.status_code.startswith("4")) or str(response.status_code).startswith("5"):
                 error_response_obj = {
                     "request_path": request.get_full_path(),
                     "status_code": response.status_code,
